3fd01737ee7af16932b6e5bb286a56ad44b94cf5
StefanKantardzhiev <stefan.ivailov@gmail.com>
2022-01-25 14:44:14 +0100

    add

---
+++ JS-Fundamentals - Arrays/Exercises/equalSums.js
@@ -0,0 +1,29 @@
+function maxSequenceOfEqualElements(arr) {
+    let brCount = 0;
+    let firstSequence = [];
+    let brFirstSequence = 0;
+
+    for (let i = 0; i < arr.length; i++) {
+        brCount = 1;
+        for (let j = i + 1; j < arr.length; j++) {
+            if (arr[i] == arr[j]) {
+                brCount++
+            } else {
+                break;
+            }
+
+        }
+        if (brFirstSequence < brCount) {
+            brFirstSequence = brCount;
+            firstSequence[0] = arr[i];
+        }
+    }
+    let curSec = firstSequence[0];
+    firstSequence = [];
+    for (let i = 1; i <= brFirstSequence; i++) {
+        firstSequence.push(curSec)
+    }
+    console.log(firstSequence.join(" "))
+
+}
+maxSequenceOfEqualElements([1,2,2,3,5,5,6])
