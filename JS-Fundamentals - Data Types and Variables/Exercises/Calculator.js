bd87503e6ea599989483fd31998750282cda9f81
StefanKantardzhiev <stefan.ivailov@gmail.com>
2022-01-29 14:45:50 +0100

    add

---
+++ JS-Fundamentals - Data Types and Variables/Exercises/Calculator.js
@@ -0,0 +1,12 @@
+function lowerOrUpper(num1, sign, num2){
+
+    switch(sign){
+        case "+" : console.log((num1+num2).toFixed(2));
+        break;
+        case "-" : console.log((num1-num2).toFixed(2))
+        break;
+        case "*" : console.log((num1*num2).toFixed(2))   
+        break;
+        case "/" : console.log((num1/num2).toFixed(2))
+    }
+}
